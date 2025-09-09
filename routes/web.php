c8de4507f7d8c452b470f0b0ea11eb30a1d1be94
Adrianus Aryo <wicaksonoadrianus5@gmail.com>
2025-09-09 14:59:59 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Models\Order;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    $order = (new Order())->sumOrderPrice();
-    return $order;
-});
+// Route::get('/', function () {
+//     $order = (new Order())->sumOrderPrice();
+//     return $order;
+// });
